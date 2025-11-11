98c09a5ec17640b9efdc705ee19f963d0946162e
ddanta-DCG <ddanta@dantacapitalgroup.com>
2025-11-11 18:32:42 +0000

    Bug fixes

--- app/vendor/onboarding/page.tsx
+++ app/vendor/onboarding/page.tsx
@@ -1,8 +1,9 @@
 import VendorOnboardingPrototype from "@/components/vendor/VendorOnboardingPrototype";
 
-export default function Page() { 
-return (
+export default function Page() {
+  return (
     <main className="bg-slate-50 py-8">
       <VendorOnboardingPrototype />
     </main>
-  );}
+  );
+}
